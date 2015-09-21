9ea1e0a5c604c41c943a2368a17c7c46f6dad706
あまみや ゆうこ <1@1234.sh>
2015-09-21 22:16:53 +0800

    logout after login test

--- test/Topicomb.Forum.Test/AccountControllerTests.cs
+++ test/Topicomb.Forum.Test/AccountControllerTests.cs
@@ -56,5 +56,35 @@ namespace Topicomb.Forum.Test
             Assert.Equal(System.Net.HttpStatusCode.Redirect, result.StatusCode);
             Assert.Equal("/Account/LoginSuccess", result.Headers.Location.OriginalString);
         }
+        
+        [Fact(Skip = "The frontend works are not finished.")]
+        public async Task Logout_After_Login_Test()
+        {
+            // Act 1
+            var result = await client.GetAsync("/Account/Login");
+
+            // Arrange
+            var cookieToken = RetrieveAntiforgeryCookie(result);
+            var csrf = RetrieveAntiforgeryToken(await result.Content.ReadAsStringAsync(), "Account/Login");
+
+            // Act 2
+            var request = new HttpRequestMessage(HttpMethod.Post, "/Account/Login");
+            request.Headers.Add("Cookie", cookieToken.Key + "=" + cookieToken.Value);
+            request.Content = PostUrl(new { Username = "admin", Password = "123456", __RequestVerificationToken = csrf, RememberMe = "false" });
+            result = await client.SendAsync(request);
+
+            // Assert
+            Assert.Equal(System.Net.HttpStatusCode.Redirect, result.StatusCode);
+            Assert.Equal("/Account/LoginSuccess", result.Headers.Location.OriginalString);
+            
+            // Act 3
+            request = new HttpRequestMessage(HttpMethod.Post, "/");
+            request.Headers.Add("Cookie", cookieToken.Key + "=" + cookieToken.Value);
+            request.Content = PostUrl(new { __RequestVerificationToken = csrf });
+            result = await client.SendAsync(request);
+            
+            // Assert
+            Assert.Equal(System.Net.HttpStatusCode.Redirect, result.StatusCode);
+        }
     }
 }
